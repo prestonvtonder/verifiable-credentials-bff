45521c1d08dedc09d4665f7529f4fbf97865dcc5
Preston van Tonder <p.vantonder@tuks.co.za>
2022-05-22 10:35:59 +0000

    feat: add public key type

--- src/keys/types.d.ts
+++ src/keys/types.d.ts
@@ -3,3 +3,10 @@ import { RequestContext } from "../types";
 export interface KeyManager {
   (did: string, ctx: RequestContext): Promise<string>
 }
+
+export interface PublicKey {
+  id: string
+  type: string
+  controller: string
+  publicKeyBase58: string
+}
